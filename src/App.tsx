9de07f2ebb4412ff911f98fbb757c6cc78fe72a5
Alexander R. Torrijos <artorrijos@gmail.com>
2022-05-11 20:00:52 +0800

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Dependencies
+import { useState, useEffect, useMemo, useCallback } from "react";
+
+// Components
+import List, { Todo } from "./List";
+
+const initialTodos = [
+  { id: 1, task: "Go shopping" },
+  { id: 2, task: "Pay the electricity bill" },
+];
 
 function App() {
+  const [todoList, setTodoList] = useState(initialTodos);
+  const [task, setTask] = useState("");
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    // console.log("Rendering <App />");
+  });
+
+  const printTodoList = useCallback(() => {
+    console.log("Changing todoList", todoList);
+  }, [todoList]);
+
+  useEffect(() => {
+    printTodoList();
+  }, [todoList, printTodoList]);
+
+  const handleCreate = () => {
+    const newTodo = {
+      id: Date.now(),
+      task,
+    };
+
+    // Pushing the new todo to the list
+    setTodoList([...todoList, newTodo]);
+
+    // Resetting input value
+    setTask("");
+  };
+
+  const handleSearch = () => {
+    setTerm(task);
+  };
+
+  const handleDelete = useCallback(
+    (taskId: number) => {
+      const newTodoList = todoList.filter((todo: Todo) => todo.id !== taskId);
+      setTodoList(newTodoList);
+    },
+    [todoList]
+  );
+
+  const filteredTodoList = useMemo(
+    () =>
+      todoList.filter((todo: Todo) => {
+        // console.log("Filtering...");
+        return todo.task.toLowerCase().includes(term.toLowerCase());
+      }),
+    [term, todoList]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+
+      <button onClick={handleCreate}>Create</button>
+      <button onClick={handleSearch}>Search</button>
+
+      <List todoList={filteredTodoList} handleDelete={handleDelete} />
+    </>
   );
 }
 
